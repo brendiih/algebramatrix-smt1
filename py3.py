0e653e4e26bd111a6d3bb7eefeb41a5388ee3a24
brendiih <brendiih13@gmail.com>
2026-09-13 19:36:03 +0700

    Reintroduce calculations and print statements

--- py3.py
+++ py3.py
@@ -1,4 +1,5 @@
 import math
+#praktikum day 1
 x = ((math.log(8,2))+(math.exp(3)))/(math.log(100,10))+((math.sqrt(121))/10)
 print(x)
 y = ((math.log(100))/(math.sqrt(10000)))+((((math.sqrt(81))*(math.sqrt(81)))**3)**4)/30
